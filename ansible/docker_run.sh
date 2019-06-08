7fdb7940a258722c891db87141572a3469623bfe
AravindK218 <aravindkumpatla218@gmail.com>
2019-06-08 22:31:36 +0000

    2.6V

--- ansible/docker_run.sh
+++ ansible/docker_run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build /var/lib/jenkins/workspace/Integration/ansible/ -t test:v1
-docker run -itd -v /var/lib/jenkins/workspace/Integration/index.html:/var/www/html/ -p 80:9234 test:v1
+docker run -itd -v /var/www/html:var/lib/jenkins/workspace/Integration/ -p 80:9234 test:v1
